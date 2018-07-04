e214fc1b8187a901e8cb0bb7a5a9f804c563b392
Dong Zhou <zhou.dong@gmail.com>
2018-07-04 00:21:41 -0400

    finish /user/ access point

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class CKCer(models.Model):
+    user = models.OneToOneField(User)
+    gender = models.BooleanField(blank=False)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    gender = serializers.BooleanField(source='ckcer.gender')
+    phone = serializers.BooleanField(source='ckcer.phone')
 
     class Meta:
         model = User
-        fields = ('url', 'email',  )
+        fields = ('url', 'email', 'first_name', 'last_name',
+                  'gender', 'phone',)
